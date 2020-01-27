d6d4cf00a15f8b0007c24c56ad4164fc4b25cb9e
“Muhammad <mmuttaki@uoguelph.ca>
2020-01-27 05:21:21 -0500

    Added ReadMe.md

---
+++ ReadeMe.md
@@ -0,0 +1,14 @@
+This program reads in two files and prints out the words in separate lines. Unfortunately I was unable to code the following functions:
+create_alphabetical_array() 
+print_words_alphabetically()
+
+My code will only print out words in a separate line and count the number of words starting with a particular letter. 
+
+To compile the code, type the following in your commandline: 
+make alphabetical_printing
+
+To run the code, type the following in your commandline: 
+./alphabetical_printing
+
+To clear all executables, type the following in your commandline: 
+make clean
--- alphabetical_printing.c
+++ alphabetical_printing.c
@@ -1,3 +1,25 @@
+/*
+
+File: alphabetical_printing.c
+Author: Muhammad Muttaki
+Date: 27/01/2020
+Course: CIS 2500
+Description: This program reads in two files and prints out the words in separate lines. Unfortunately I was unable to code the following functions:
+            create_alphabetical_array() 
+            print_words_alphabetically()
+            
+            This program includes the following functions:
+            int file_size(FILE *fp);
+            char *read_words(FILE *fp);
+            void print_words(char words[]);
+            int *alphabatical_word_count(char words[]);
+            void print_alphabetical_word_count(int word_numbers[]);
+            void free_alphabetical_array(char array[]);
+            void free_word_counts(int word_count[]);    (I wrote this function for my own ease. Writing this function wasn't mandatory.)
+
+*/
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -18,6 +40,8 @@ int main(int argc, char *argv[])
     free_word_counts(alphabatical_word_count(read_words(fp)));
     free_alphabetical_array(read_words(fp));
     
+    printf("\n");
+
     /* Reading and printing file a1_words_more.txt */
     is_first_part_done = true;
     read_words(fp);      
